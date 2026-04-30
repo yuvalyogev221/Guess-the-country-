dc881d3c8d30df22900be03b401e78342b6b962a
yuvalyogev221 <167207530+yuvalyogev221@users.noreply.github.com>
2026-05-01 01:04:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
 from logic import Comparison
 import os
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def home():
